c758e17de9db0ec9f64d045cbfb619e59d96bb43
Henrique Ortiz <henriqueortiz.1@gmail.com>
2023-04-27 16:34:15 -0300

    Add private_dashboards_example

---
+++ public_dashboards_example copy.py
@@ -0,0 +1,34 @@
+from tableau_dashboard_looper import TableauDashboardLooper
+
+DASHBOARDS = {
+    'dashboards': [
+        {
+            "url": "",
+            "update_every": 600,
+            "how_long_to_stay": 180,
+            "updated_at": ""
+        },
+        {
+            "url": "",
+            "update_every": 600,
+            "how_long_to_stay": 180,
+            "updated_at": ""
+        }
+    ]
+}
+
+def main():
+    
+    tableau = TableauDashboardLooper(
+        username = '-- ADD USERNAME HERE --',
+        password = '-- ADD PASSWORD HERE --',
+        tableau_login_url = '-- ADD YOUR TABLEAU INSTANCE URL HERE --',
+        dashboards=DASHBOARDS
+    )
+
+    tableau.start_browser()
+    tableau.open_dashboards()
+    tableau.loop_through_tabs(refresh=False)
+
+if __name__ == '__main__':
+    main()
